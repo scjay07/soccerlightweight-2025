90738a4bff223b78ae4a907c2f8746575438eb24
juliay <143917640+scjay07@users.noreply.github.com>
2025-02-27 17:57:58 -0300

    Update esproboantigo30.01.ino

--- ARDUINO/esproboantigo30.01/esproboantigo30.01.ino
+++ ARDUINO/esproboantigo30.01/esproboantigo30.01.ino
@@ -1,22 +1,17 @@
-//GK TA COM -1 NO 6 IR
-//ESP GK JOGO 2
-
 #include <HardwareSerial.h>
-#include <LCD_I2C.h>
 HardwareSerial mySerial(2); // define a Serial for UART2
 //ESP
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);  // O endereço padrão é 0x28, mas pode variar
+#include <Wire.h>    //Include wire library 
 
 const int mySerialRX = 16; // na esp32 isso eh o rx2 e o tx2
 const int mySerialTX = 17;
+#include <Adafruit_Sensor.h>'
+#include <Adafruit_BNO055.h>
 
-LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
-
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 int angulogiro;
+int anguloyaw;
+
 float angleZ = 0;          // Estimativa do ângulo
 float biasZ = 0;           // Desvio de bias
 float rateZ = 0;           // Taxa de mudança angular
@@ -24,8 +19,6 @@ float P = 1;               // Matriz de covariância
 float Q = 0.01;            // Process noise (incerteza do processo)
 float R = 0.01;             // Medição de ruído (incerteza das medições)
 float K = 0;               // Ganho de Kalman
-
-
 float Kp = 6;
 float Kd = 1.25;
 float Ki = 10;
@@ -41,12 +34,9 @@ int x;
 
 int IR[16];
 int valor[16];
-int ori[5] = {0, 0, 0, 0, 0};
 const int portas[16] =  {15, 4, 5, 18, 19, 36, 39, 23, 13, 14, 26, 25, 33, 32, 35, 34};
-//const int minimo[16] = {3700,4532,4260,4442,-1,4357,4249,4247,4694,4242,4527,4605,4468,4290,4605,4064};
-//const int minimo[16] = {5201,4841,4587,4725,-1,4418,4511,5421,4551,4612,4621,5915,4802,5588,5934,4583};
-const int minimo[16] = {4801, 4841, 4587, 4725, -1, 4418, 4511, 5421, 4551, 4612, 4621, 5915, 4802, 5588, 5934, 4583};
-//5253,6766,5952,5617,-1,5968,5362,6182,6566,5545,5858,5686,5601,8001,5767,4803
+const int minimo[16] = {4026, 4114, 4302, 4617, 4413, 4241, 4310, 4664, 4928, 4030, 4330, 4867, 4082, 4502, 4128, 4280};
+//4332,4173,4341,4493,-1,2791,4398,4194,4283,4167,4161,4097,4098,4422,4255,4847
 
 //
 //int valor1;
@@ -59,7 +49,7 @@ const int minimo[16] = {4801, 4841, 4587, 4725, -1, 4418, 4511, 5421, 4551, 4612
 //int valor8;
 
 int bola;
-
+bool perto;
 const int MAX = 10000;
 
 int16_t ax, ay, az, gy, gx, gz;
@@ -89,7 +79,8 @@ int leitura()
   return sensor_min;
 }
 
-void setup() {
+void setup()
+{
   for (int i = 0; i < 16; i++)
   {
     //    if (i == 9)
@@ -101,21 +92,15 @@ void setup() {
   {
     for (int j = 0; j < MAX; j++) IR[i] = 0;
   }
-  Serial.begin(115200);
-  mySerial.begin(115200, SERIAL_8N1, mySerialRX, mySerialTX);
+  Serial.begin(9600);
+  mySerial.begin(9600, SERIAL_8N1, mySerialRX, mySerialTX);
   mySerial.setTimeout(5);
   Wire.begin();
-  if (!bno.begin()) {
+  if (!bno.begin())
+  {
     Serial.print("Não foi possível encontrar o sensor BNO055");
     while (1);
   }
-
-  bno.setMode(OPERATION_MODE_COMPASS);
-
-
-  Serial.println("Iniciando o BNO055...");
-  
-
 }
 
 //bool direita = 1;
@@ -140,9 +125,6 @@ void loop() {
 
   // Leitura bruta do eixo Z
   float rawZ = event.orientation.x;
-  
-//  Serial.print("raw: ");
-//  Serial.println(rawZ);
 
   // Taxa de mudança angular
   rateZ = rawZ - angleZ;
@@ -163,23 +145,6 @@ void loop() {
   P = (1 - K) * P;
 
   angulogiro =  angleZ;
-  //
-  //  if (anguloyaw >= 360)
-  //  {
-  //    anguloyaw = anguloyaw % 360;
-  //  }
-  //  if (anguloyaw < 0 && anguloyaw >= -360);
-  //  {
-  //    anguloyaw = anguloyaw % 360;
-  //  }
-
-  //  error = target - anguloyaw;// proportional
-  //  integral = integral + error; //integral
-  //  derivative = error - last_error; //derivative
-  //  last_error = error;
-
-
-  //  angle = (error * Kp); // + (derivative* Kd);w
 
 
   for (int i = 0, min_ = 10000; i < 16; i++)
@@ -194,82 +159,23 @@ void loop() {
     }
   }
 
-   if (angulogiro >= 180 ) angulogiro -= 360;
-   
-  //    for (int i = 0; i < qtd_leituras; i++)
-  //    {
-  //      lista_min[i] = leitura();
-  //    }
-  //
-  //    for (int i = 0; i < qtd_leituras; i++) {
-  //      contagem[i] = 0;
-  //    }
-  //
-  //    for (int i = 0; i < qtd_leituras; i++) {
-  //      contagem[lista_min[i]]++;
-  //    }
-  //
-  //    int maxContagem = 0;
-  //    for (int i = 0; i < qtd_leituras; i++) {
-  //      if (contagem[lista_min[i]] > maxContagem) {
-  //        maxContagem = contagem[lista_min[i]];
-  //        index = lista_min[i];
-  //      }
-  //    }
-  //  Serial.println(index);
-  //  Serial.println(bola);
+  if (angulogiro > 180) angulogiro -= 360;
 
-  if ((IR[0] < 2 && IR[15] < 2))
+  bola = 1024;
+  for (int i = 4; i <= 12 ; i++)
   {
-    bola = 1024; //true
-    //while (!mySerial.available());
-    if (mySerial.available())
+    if (IR[i] > 20)
     {
-      x = mySerial.parseInt();
-      //Serial.println(x);
-      if (x != 0)
-      {
-
-        //Serial.println(x);
-        if (x == 333)
-        {
-          //          if (direita == 1 || frente == 1) {
-          // Serial.println("esquerda");
-          mySerial.println(333); //esquerda
-          //            esquerda = 1;
-          //            direita = 0;
-          //            frente = 0;
-          // }
-
-        }
-        else if (x == 666) {
-          // if (esquerda == 1 || frente == 1) {
-          //  Serial.println("direita");
-          mySerial.println(666); //direita
-          //            direita = 1;
-          //            esquerda = 0;
-          //            frente = 0;
-          //          }
-        }
-        else if (x == 444)
-        {
-          //   if (direita == 1 || esquerda == 1) {
-          //    Serial.println("frente");
-          mySerial.println(444); //frente
-          //            frente = 1;
-          //            direita = 0;
-          //            esquerda = 0;
-          //          }
-        }
-        mySerial.flush();
-      }
+      bola = 1023;
+      Serial.println(bola);
+      break;
     }
+  }
 
-  }
-  else
-  {
-    bola = 1023; //false
-  }
+
+  mySerial.println(bola);
+  Serial.println(bola);
+
   switch (index)
   {
     case 0: //IR1
@@ -348,11 +254,9 @@ void loop() {
   {
     IR[k] = 0;
   }
-
-  //mySerial.println(bola);
   Serial.println (angulogiro);
   mySerial.println(angulogiro + 3000);
   //mySerial.println(angle + 6000);
   //Serial.println(angle);
-
 }
+
